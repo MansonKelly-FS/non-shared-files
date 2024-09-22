1367e1ef9eeaffaf5666d414151c2f66c6c84889
Kelly Manson <klmanson@student.fullsail.edu>
2024-09-22 10:19:48 -0600

    non-shared secret file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Setup
+To run the application, you need to create a .env file in the root directory of the project. Inside this file, define a SECRET_KEY variable like this:
+
+`SECRET_KEY=yourSecretKeyHere`
+
+This key is required for the application to function correctly.
---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const fs = require("fs");
+let directory_name = "./";
+let filenames = fs.readdirSync(directory_name);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  f = "";
+  filenames.forEach((file) => {
+    f += file + "\n";
+  });
+  res.end(f);
+});
+
+// Non-Shared Secret Files
+const secret_key = process.env.SECRET_KEY;
+console.log(`my secret password is ${secret_key}`);
+
+server.listen(port, hostname, () => {});
